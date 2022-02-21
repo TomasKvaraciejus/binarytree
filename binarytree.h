e630b8991dd3bae726b36e87e6c80d5df5996bda
Tomas Kvaraciejus <95697934+TomasKvaraciejus@users.noreply.github.com>
2022-02-21 12:10:29 +0200

    Update binarytree.h

--- binarytree.h
+++ binarytree.h
@@ -1,46 +1,46 @@
 #ifndef BINARYTREE_H
 #define BINARYTREE_H
 
-struct node
+struct node // Individual nodes of AVL tree
 {
 	int data, height;
 	struct node* leftNode, * rightNode;
 };
 typedef struct node node;
 
-struct binaryTree
+struct binaryTree // Tree
 {
 	int size;
 	node* top;
 };
 typedef struct binaryTree binaryTree;
 
-binaryTree* createBinaryTree(int value);
+binaryTree* createBinaryTree(int value); // returns pointer to new binary tree with root of given value
 
-node* createNode(int value);
+node* createNode(int value); // returns pointer to node with given value
 
-node* addItem(int value, node* _n);
+node* addItem(int value, node* _n); // recursive function that inserts item
 void addTreeItem(int value, binaryTree* _binaryTree);
 
-node* balanceTree(int value, node* _n);
+node* balanceTree(int value, node* _n); // function that adjusts tree to fit AVL type
 
-node* rotateLeft(node* _n);
+node* rotateLeft(node* _n); // function used in balancing
 
-node* rotateRight(node* _n);
+node* rotateRight(node* _n); // function used in balancing
 
-node* removeItem(int value, node* _n);
+node* removeItem(int value, node* _n); // recursive function that removes item
 void removeTreeItem(int value, binaryTree* _binaryTree);
 
-void removeTree(binaryTree * _binaryTree);
+void removeTree(binaryTree * _binaryTree); // function that removes entire tree
 node* removeInOrder(node* _n, node* _parent);
 
-void printPathToItem(int value, binaryTree* _binaryTree);
+void printPathToItem(int value, binaryTree* _binaryTree); // prints path to given value
 
-void printInOrder(node* _n);
+void printInOrder(node* _n); // prints tree values in order
 
-int getMax(int a, int b);
+int getMax(int a, int b); // function used in balancing
 
-int getHeight(node* _n);
+int getHeight(node* _n // function used in balancing
 
 #endif
 
